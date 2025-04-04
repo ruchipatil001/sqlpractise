024b2646550418607103b6fb398792a6622bec08
ruchipatil001 <ruchicdac25@gmail.com>
2025-04-04 23:18:08 +0530

    Create examples.sql

---
+++ examples.sql
@@ -0,0 +1,13 @@
+delimiter //
+  
+ create function calcBonus3(empid int) returns decimal
+ begin 
+ declare bonus decimal(10,2);
+ declare vsal decimal(10,2);
+ select sal into vsal from emp
+ where empno=empid;
+ set bonus=(vsal+vsal*1.10);
+ return bonus;
+ end //
+   
+ select empno,ename,sal,calBonus3(empno) from emp;
